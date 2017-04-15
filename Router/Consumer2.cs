e437d799043c0556167b0fb7d8959bfbbecbb794
RyanWen <wenld0849@hotmail.com>
2017-04-15 22:27:10 +0800

    comment out the test code

--- Router/Consumer2.cs
+++ Router/Consumer2.cs
@@ -18,11 +18,11 @@ namespace Router
             Console.WriteLine("Consumer2 is handling EvtMsg1 {0}", msg.Para3);
         }
 
-        [ExpectEvtMsg(typeof(EvtMsg2))]
-        public void HandleEvtMsg22(EvtMsg2 msg)
-        {
-            Console.WriteLine("Consumer1 is handling EvtMsg2 {0}", msg.Para4);
-        }
+        //[ExpectEvtMsg(typeof(EvtMsg2))]
+        //public void HandleEvtMsg22(EvtMsg2 msg)
+        //{
+        //    Console.WriteLine("Consumer1 is handling EvtMsg2 {0}", msg.Para4);
+        //}
 
         public void InvokeReq1()
         {
